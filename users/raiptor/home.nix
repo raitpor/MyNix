12ac32b29a928d2916c756f069e83eb09f1e77cd
raitpor <928890461@qq.com>
2026-07-06 23:01:28 +0800

    补充引用

--- users/raiptor/home.nix
+++ users/raiptor/home.nix
@@ -24,6 +24,7 @@ in
     ./programs/neovim.nix
     ./programs/git.nix
     ./programs/noctalia.nix
+    ./programs/steam.nix
   ];
 
   xdg.configFile."niri/config.kdl".source = ./configs/niri/config.kdl;
