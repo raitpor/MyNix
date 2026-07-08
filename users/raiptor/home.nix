50d5c1986e9f85ce346e16e3520cb012356a6c39
raitpor <928890461@qq.com>
2026-07-08 10:20:20 +0800

    临时同步

--- users/raiptor/home.nix
+++ users/raiptor/home.nix
@@ -7,11 +7,15 @@
 
 let
   unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
+  novapkgs = inputs.nixpkgs-nova.packages.${pkgs.system};
 in
 {
   home.username = "raiptor";
   home.homeDirectory = "/home/raiptor";
   home.stateVersion = "26.05";
+
+  # 允许unfree
+  nixpkgs.config.allowUnfree = true;
 
   # 导入官方提供的 Git 和 Fish 模块
   programs.home-manager.enable = true;
@@ -79,5 +83,7 @@ in
     bottles
 
     calibre
+
+    novapkgs.reasonix-desktop
   ];
 }
